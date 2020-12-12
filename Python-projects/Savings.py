f6f6e8bae18f037f591c18b1b8e0efec602a882a
Phillip <phillipmgonzalez77@gmail.com>
2020-12-11 18:53:19 -0700

    Create Savings.py
    Just a simple savings program, it generates one of the dollar amounts. The outcome is the amount you're supposed to put in your savings.

---
+++ Python-projects/Savings.py
@@ -0,0 +1,5 @@
+#For the amount shown I will deposit in my savings account
+import random
+amount = ['$50', '$75', '$125', '$175', '$200']
+
+print(f"This paycheck we are going to put {random.choice(amount)} in your savings")
